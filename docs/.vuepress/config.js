291fcd85d0881de6c5c06bc01f0036d9d5a86f1b
Satheesh Valluru <vallurusateesh2009@gmail.com>
2020-01-22 21:19:53 +0530

    Initial Commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'CloudAction',
+    base: '/public/docs/',
+    dest: 'public/docs',
+    description: 'Just playing around',
+    search: false,
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Page One', link: '/pageOne/' },
+            { text: 'Page Two', link: '/pageTwo/' }
+        ]
+    }
+}
