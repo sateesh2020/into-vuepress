850a6059fdd49b66d2c7e8a0cd989ea1fc00e89a
Satheesh Valluru <vallurusateesh2009@gmail.com>
2020-02-04 15:07:37 +0530

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,10 +5,33 @@ module.exports = {
     description: 'Just playing around',
     search: false,
     themeConfig: {
+        displayAllHeaders: true,
         nav: [
             { text: 'Home', link: '/' },
-            { text: 'Page One', link: '/pageOne/' },
-            { text: 'Page Two', link: '/pageTwo/' }
-        ]
+            { text: 'Guide', link: '/guide/' },
+            { text: 'Getting Started', link: '/gettingStarted/' }
+        ],
+        sidebar: {
+            '/guide/': [
+                {
+                    title: 'Guide',
+                    collapsable: false,
+                    children: [
+                        '',
+                        'intro'
+                    ]
+                }
+            ],
+            '/gettingStarted/': [
+                {
+                    title: 'Getting Started',
+                    collapsable: false,
+                    children: [
+                        '',
+                    ]
+                }
+            ]
+
+        }
     }
 }
--- docs/Readme.md
+++ docs/Readme.md
@@ -1,14 +1,13 @@
 ---
 home: true
-heroText: API & Integration with Speed of Serverless
-tagline: subtitle
+heroText: Welcome to Cloud Action Docs
 actionText: Play →
-actionLink: /pageOne/
+actionLink: /guide/
 features:
-- title: Simplicity First
+- title: Title 1
   details: Minimal setup with markdown-centered project structure helps you focus on writing.
-- title: Vue-Powered
+- title: Title 2
   details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
-- title: Performant
+- title: Title 3
   details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
 ---
